0aaf1425f04b721c759f8e2f0d6b39409ddae49a
oowenn <a4shi@ucsd.edu>
2025-01-23 11:34:16 -0800

    lab 3 step 3

--- global.js
+++ global.js
@@ -1,5 +1,42 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
+
+// const navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+// }
+
+let pages = [
+    { url: '../home/home.html', title: 'Home' },
+    { url: '../projects/projects.html', title: 'Projects' },
+    { url: '../contact/contact.html', title: 'Contact' },
+    { url: '../CV/cv.html', title: 'CV' },
+    { url: 'https://github.com/oowenn/portfolio', title: 'GitHub' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+    nav.append(a);}
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
