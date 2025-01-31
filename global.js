5eeaf5139d592a233f8546b546e3abd7b5643411
oowenn <a4shi@ucsd.edu>
2025-01-31 14:48:00 -0800

    lab4 step 4

--- global.js
+++ global.js
@@ -91,4 +91,8 @@ export function renderProjects(project, containerElement, headingLevel = 'h2') {
             `;
         containerElement.appendChild(article);
     });
+};
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
 }
--- home/index.js
+++ home/index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects} from '../global.js';
+import { fetchJSON, renderProjects, fetchGitHubData} from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -7,3 +7,18 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h3');
+
+const githubData = await fetchGitHubData('oowenn');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
